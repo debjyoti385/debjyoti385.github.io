a4cda8542debab745b448e087768bf5f5111cb65
Debjyoti <debjyotipaul385@gmail.com>
2014-11-24 18:47:01 +0530

    removing static dartnews

--- dartnews/crawling/findAllAreas.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-outputfile="delhiAreas"
-lat=28.5
-while(lat<=28.7):
-	lon=77.2
-	while(lon<=77.3):
-		os.system("python customGeoNameLookup.py "+str(lat)+" "+str(lon)+" "+outputfile)
-		lon+=0.001
-	lat+=0.01
